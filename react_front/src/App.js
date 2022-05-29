060578a7d918d3c33588f08e8709d576f293bffb
Dmitrii <kachkindmitry@gmail.com>
2022-05-29 20:47:03 +0300

    routes  added

--- react_front/src/App.js
+++ react_front/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 
+
 function App() {
   return (
     <div className="App">
