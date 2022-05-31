7543c7979dfba020a689d1e98333bc854db5f47f
Dmitrii <kachkindmitry@gmail.com>
2022-05-31 17:12:54 +0300

    test images path

--- react_front/src/pages/shop/shop.data.js
+++ react_front/src/pages/shop/shop.data.js
@@ -7,36 +7,36 @@ const SHOP_DATA = [
       {
         id: 1,
         name: 'Brown Brim',
-        // imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
-        imageUrl: "/images/shop-img/hats/brown-brim.png",
+        imageUrl: 'https://i.ibb.co/ZYW3VTp/brown-brim.png',
+        // imageUrl: "/images/shop-img/hats/brown-brim.png",
         price: 25
       },
       {
         id: 2,
         name: 'Blue Beanie',
-        // imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
-        imageUrl: "/images/shop-img/hats/blue-beanie.png",
+        imageUrl: 'https://i.ibb.co/ypkgK0X/blue-beanie.png',
+        // imageUrl: "/images/shop-img/hats/blue-beanie.png",
         price: 18
       },
       {
         id: 3,
         name: 'Brown Cowboy',
-        // imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
-        imageUrl: "/images/shop-img/hats/brown-cowboy.png",
+        imageUrl: 'https://i.ibb.co/QdJwgmp/brown-cowboy.png',
+        // imageUrl: "/images/shop-img/hats/brown-cowboy.png",
         price: 35
       },
       {
         id: 4,
         name: 'Grey Brim',
-        // imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
-        imageUrl: "/images/shop-img/hats/grey-brim.png",
+        imageUrl: 'https://i.ibb.co/RjBLWxB/grey-brim.png',
+        // imageUrl: "/images/shop-img/hats/grey-brim.png",
         price: 25
       },
       {
         id: 5,
         name: 'Green Beanie',
         // imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-        imageUrl: "/images/shop-img/hats/green-beanie.png",
+        // imageUrl: "/images/shop-img/hats/green-beanie.png",
         price: 18
       },
       {
@@ -77,8 +77,8 @@ const SHOP_DATA = [
       {
         id: 10,
         name: 'Adidas NMD',
-        // imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-        imageUrl: "/images/shop-img/sneakers/adidas-nmd.png",
+        imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+        // imageUrl: "/images/shop-img/sneakers/adidas-nmd.png",
         price: 220
       },
       {
@@ -253,28 +253,28 @@ const SHOP_DATA = [
         id: 32,
         name: 'Black & White Longsleeve',
         // imageUrl: 'https://i.ibb.co/55z32tw/long-sleeve.png',
-        imageUrl: "/images/shop-img/mens/long-sleeve.png",
+        // imageUrl: "/images/shop-img/mens/long-sleeve.png",
         price: 25
       },
       {
         id: 33,
         name: 'Pink T-shirt',
-        // imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
-        imageUrl: "/images/shop-img/mens/pink-shirt.png",
+        imageUrl: 'https://i.ibb.co/RvwnBL8/pink-shirt.png',
+        // imageUrl: "/images/shop-img/mens/pink-shirt.png",
         price: 25
       },
       {
         id: 34,
         name: 'Jean Long Sleeve',
         // imageUrl: 'https://i.ibb.co/VpW4x5t/roll-up-jean-shirt.png',
-        imageUrl: "/images/shop-img/mens/roll-up-jean-shirt.png",
+        // imageUrl: "/images/shop-img/mens/roll-up-jean-shirt.png",
         price: 40
       },
       {
         id: 35,
         name: 'Burgundy T-shirt',
         // imageUrl: 'https://i.ibb.co/mh3VM1f/polka-dot-shirt.png',
-        imageUrl: "/images/shop-img/mens/polka-dot-shirt.png",
+        // imageUrl: "/images/shop-img/mens/polka-dot-shirt.png",
         price: 25
       }
     ]
