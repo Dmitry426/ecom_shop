be0293ed804ce000de93f716940643ada172a287
Dmitrii <kachkindmitry@gmail.com>
2022-06-18 18:23:46 +0300

    format:  perttier

--- react_front/src/pages/shop/shop.component.jsx
+++ react_front/src/pages/shop/shop.component.jsx
@@ -5,28 +5,25 @@ import SHOP_DATA from "./shop.data";
 import PreviewCollection from "../../components/preview-collection/preview-collection.component";
 
 class ShopPage extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-
-        this.state = {
-            collections:SHOP_DATA
-        };
-
-    }
-    render() {
-        const {collections} = this.state;
-        return (
-            <div className="shop-page">
-                {
-                    collections.filter((item , idx ) => idx < 4 ).map(({id, ...otherCollectionProps}) => (
-                        <PreviewCollection key={id} {...otherCollectionProps } />
-                    ))
-                }
-
-        </div>
-        )
-    }
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className="shop-page">
+        {collections
+          .filter((item, idx) => idx < 4)
+          .map(({ id, ...otherCollectionProps }) => (
+            <PreviewCollection key={id} {...otherCollectionProps} />
+          ))}
+      </div>
+    );
+  }
 }
 
-export default ShopPage
+export default ShopPage;
