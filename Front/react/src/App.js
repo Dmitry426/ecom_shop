c0cae4218d972f01d18106fc1c6795414d2749cb
Dmitrii <kachkindmitry@gmail.com>
2022-06-28 23:17:11 +0300

    chore: auth Route added

--- Front/react/src/App.js
+++ Front/react/src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
-import SingIn from "./pages/sign-in/sign-in.component";
+import SingIn from "./pages/authentication/authentication.component";
 import Navigation from "./pages/navigation/navigation.component";
 
 function App() {
@@ -13,7 +13,7 @@ function App() {
         <Route path="/" element={<Navigation />}>
           <Route index element={<HomePage />} />
           <Route path="shop" element={<ShopPage />} />
-          <Route path="sign" element={<SingIn />} />
+          <Route path="auth" element={<SingIn />} />
         </Route>
       </Routes>
     </div>
