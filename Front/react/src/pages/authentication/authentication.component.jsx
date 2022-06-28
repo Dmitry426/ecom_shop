393af046e66d6748e49a6bec32942bbae0a22390
Dmitrii <kachkindmitry@gmail.com>
2022-06-28 23:19:20 +0300

    feature: auth page component

--- Front/react/src/pages/authentication/authentication.component.jsx
+++ Front/react/src/pages/authentication/authentication.component.jsx
@@ -1,10 +1,12 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
 const SingIn = () => {
   return (
     <div>
       <h1>Render</h1>
       <SignUpForm />
+      <SignInForm />
     </div>
   );
 };
