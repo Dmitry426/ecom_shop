05162e358422c74b4b1b998d59647adb4f724215
Dmitrii <kachkindmitry@gmail.com>
2022-06-29 19:20:43 +0300

    chore: auth component with styles

--- Front/react/src/pages/authentication/authentication.component.jsx
+++ Front/react/src/pages/authentication/authentication.component.jsx
@@ -1,10 +1,10 @@
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import "./authentication.styles.scss";
 
 const SingIn = () => {
   return (
-    <div>
-      <h1>Render</h1>
+    <div className="authentication-container">
       <SignUpForm />
       <SignInForm />
     </div>
